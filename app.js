a836e7a5e7cd944f7864d86425e5a955267df5a8
sufiyan khan <suffunew@gmail.com>
2025-11-01 20:16:19 +0530

    create new and show route and ejs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/TripoSpace";
 
@@ -17,15 +18,52 @@ main()
     console.log(err);
   });
 
-app.get("/", async (req, res) => {
-   const allListings = await Listing.find({})
-   res.render("index.ejs", {allListings});
-    
-});
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"views"));
+app.use(express.urlencoded({extended: true}));
+
 
 app.get("/", (req, res) => {
   res.send("HI I am root");
 });
+
+// INDEX
+app.get("/listings", async (req, res) => {
+   const allListings = await Listing.find({})
+   res.render("listings/index", {allListings});
+    
+});
+
+
+// NEW ROUTE
+
+app.get("/listings/new",(req,res) => {
+    res.render("listings/new");
+})
+
+
+// SHOW ROUTE
+
+app.get("/listings/:id", async(req,res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show",{listing});
+
+});
+
+// Create Route
+
+app.post("/listings",async (req,res)=>{
+    // let {title,description,image,price,country,location} = req.body;
+    // let listing = req.body.listing;
+   const newListing = new Listing(req.body.listing);
+   await newListing.save();
+   res.redirect("/listings");
+
+
+});
+
+
 
 //   app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
--- models/listing.js
+++ models/listing.js
@@ -6,10 +6,11 @@ const listingSchema = new Schema ({
         type :String,
         required : true
     },
+    description: String,
     image : {
         type :String,
         default : "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070 ",
-        
+
         set: (v) => v ==="" ? "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070 " : v,
     },
     price : Number,
