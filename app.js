52686e5da7d8cd8bcad186f4e9aa88d82b2f5b2d
sufiyan khan <suffunew@gmail.com>
2025-11-01 07:34:39 +0530

    added gitignore file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 
-MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb://127.0.0.1:27017/TripoSpace";
 
 async function main() {
   await mongoose.connect(MONGO_URL);
@@ -17,27 +17,29 @@ main()
     console.log(err);
   });
 
+app.get("/", async (req, res) => {
+   const allListings = await Listing.find({})
+   res.render("index.ejs", {allListings});
+    
+});
 
-  app.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("HI I am root");
 });
 
-
-  app.get("/listing", async (req, res) => {
-  let sampleListing = new Listing({
-    title : "My new villa",
-    description : "near beach",
-    price : 200,
-    location :"Vapi,Gujarat",
-    country : "India",
-  });
-  await sampleListing.save();
-  console.log("Sample was saved");
-  res.send("successful testing");
-});
-
+//   app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title : "My new villa",
+//     description : "near beach",
+//     price : 200,
+//     location :"Vapi,Gujarat",
+//     country : "India",
+//   });
+//   await sampleListing.save();
+//   console.log("Sample was saved");
+//   res.send("successful testing");
+// });
 
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
 });
-
