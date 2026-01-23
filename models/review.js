1592be275ea2ee7706d552a78af8e3ba78f27292
sufiyan khan <suffunew@gmail.com>
2026-01-23 23:24:20 +0530

    create review schema

---
+++ models/review.js
@@ -0,0 +1,20 @@
+const { string } = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const reviewSchema = new Schema({
+    comment : string, 
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5
+    },
+    createdAt : {
+        type : Date,
+        default : Date.now(),
+    }
+
+});
+
+module.exports = mongoose.model("Review",reviewSchema);
