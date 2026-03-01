ca97f2ad5369c04e312bf244da98b2d2ec33f3c4
sufiyan khan <suffunew@gmail.com>
2026-03-01 16:10:20 +0530

    created post login

--- routes/user.js
+++ routes/user.js
@@ -1,49 +1,62 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
-const wrapAsync = require("../utils/wrapAsync.js"); 
-const passport = require("passport"); 
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
+const {saveRedirectUrl} = require("../middleware.js");
 
-router.get("/signup",(req,res)=>{
-    res.render("users/signup.ejs")
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
 });
 
-router.post("/signup",wrapAsync(async(req,res) => {
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
     try {
-        let {username, email, password} = req.body;
-    const newUser = new User({email, username});
-    const registeredUser = await User.register(newUser, password);
-    console.log(registeredUser);
-    req.flash("success", "Welcome to TripoSpace");
-    res.redirect("/listings"); 
-    } catch(e){
-         req.flash("error", e.message);
-         res.redirect("/signup");
+      let { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.flash("success", "Welcome to TripoSpace");
+      res.redirect("/listings");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
     }
-}));
+  }),
+);
 
-router.get("/login",(req,res) => {
-    res.render("users/login.ejs");
+// routes/users.js or wherever
+
+router.get("/login",  (req, res) => {
+  res.render("users/login.ejs");
 });
 
-router.post("/login",
-    passport.authenticate("local",{
-        failureRedirect:"/login", 
-        failureFlash : true
-    }),
-    async(req,res)=>{
-     req.flash("success","Welcome Back to TripoSpace");
-     res.redirect("/listings");
-});
+router.post(
+  "/login",saveRedirectUrl,
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  (req, res) => {
+    // console.log("Login SUCCESS → redirecting to:", res.locals.redirectUrl || "/listings");
 
-router.get("/logout",(req, res) => {
-    req.logout((err) =>{
-        if(err){
-           return next(err);
-        }
-        req.flash("success", "You are Logout");
-        res.redirect("/listings");
-    });
+    let redirectTo = res.locals.redirectUrl || "/listings";
+    req.session.redirectUrl = null;  // cleanup
+
+    req.flash("success", "Welcome Back to TripoSpace!");
+    res.redirect(redirectTo);
+  }
+);
+
+router.get("/logout", (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "You are Logout");
+    res.redirect("/listings");
+  });
 });
 
 module.exports = router;
