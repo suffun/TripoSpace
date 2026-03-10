81272ba569ef232e974eb4e3a99b5bd6da55720f
sufiyan khan <suffunew@gmail.com>
2026-03-11 02:53:56 +0530

    cloud setup and store files

--- routes/listing.js
+++ routes/listing.js
@@ -7,7 +7,8 @@ const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router
   .route("/")
